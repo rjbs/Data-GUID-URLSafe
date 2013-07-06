486cd40237f317acbc6282523cd53ad0dd0ce567
Ricardo Signes <rjbs@cpan.org>
2013-07-05 21:31:01 -0400

    dzilify

--- lib/Data/GUID/URLSafe.pm
+++ lib/Data/GUID/URLSafe.pm
@@ -1,18 +1,7 @@
-
 use strict;
+use warnings;
 package Data::GUID::URLSafe;
-
-=head1 NAME
-
-Data::GUID::URLSafe - url-safe base64-encoded GUIDs
-
-=head1 VERSION
-
-version 0.004
-
-=cut
-
-$Data::GUID::URLSafe::VERSION = '0.004';
+# ABSTRACT: url-safe base64-encoded GUIDs
 
 =head1 SYNOPSIS
 
@@ -32,8 +21,6 @@ These strings are also safer for email addresses.  While the forward slash is
 legal in email addresses, some broken email address validators reject it.
 (Also, without the trailing equals signs, these strings will be shorter.)
 
-=head1 METHODS
-
 When Data::GUID::URLSafe is C<use>'d, it installs methods into Data::GUID using
 L<Sub::Exporter|Sub::Exporter>.
 
@@ -46,7 +33,7 @@ use Sub::Exporter -setup => {
   groups  => [ default => [ -all ] ],
 };
 
-=head2 as_base64_urlsafe
+=method as_base64_urlsafe
 
   my $string = $guid->as_base64_urlsafe;
 
@@ -62,7 +49,7 @@ sub as_base64_urlsafe {
   return $base64;
 }
 
-=head2 from_base64_urlsafe
+=method from_base64_urlsafe
 
   my $guid = Data::GUID->from_base64_urlsafe($string);
 
@@ -80,25 +67,5 @@ sub from_base64_urlsafe {
 
   return $self->from_base64($string);
 }
-
-=head1 AUTHOR
-
-Ricardo SIGNES, C<< <rjbs at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-GUID-URLSafe>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as
-I make changes.
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2007 Ricardo SIGNES.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
 
 1;
